a64948b4c950ae58d24cd7e51d16de0754a55e94
Anton Lyscov <anton.lyscov@yandex.ru>
2023-04-09 12:40:40 +0300

    добавлены исключения, проверка куки

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,15 +4,17 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
+  if (req.cookies.jwt) {
+    const token = req.cookies.jwt;
+    let payload;
 
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    req.user = payload;
-    next();
-  } catch (e) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    try {
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+      req.user = payload;
+      next();
+    } catch (e) {
+      next(new UnauthorizedError('Необходима авторизация'));
+    }
   }
 };
 
